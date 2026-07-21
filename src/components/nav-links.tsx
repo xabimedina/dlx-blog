0e015c6b377c276f94c4613e94dd744cb1b1c72b
xabimedina <info@despejalax.com>
2026-07-21 18:39:13 +0200

    update links

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -10,7 +10,7 @@ export function NavLinks() {
           href={SITE.mainSite}
           className='relative cursor-pointer group transition-all duration-200 text-jet'
         >
-          Estudio
+          Inicio
           <span className='absolute -bottom-1 left-1/2 w-0 h-0.5 bg-saffron transition-all duration-200 group-hover:w-full group-hover:left-0'></span>
         </a>
       </div>
